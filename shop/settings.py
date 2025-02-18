a34fd6bd5cd9dcf9cfa4dee09aaccc65de404407
unknown <kajakgm@gmail.com>
2025-02-18 17:10:56 +0530

    lastfinalcommit

--- shop/settings.py
+++ shop/settings.py
@@ -121,7 +121,8 @@ MEDIA_URL='/images/'
 MEDIA_ROOT=BASE_DIR/'static'
 STATICFILES_DIRS=[BASE_DIR/
                   'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
